eb06d729ca5bd9121eefe6d10e5e5d1a02fa18f9
dandamdandam <glue0440@gmail.com>
2024-10-31 23:14:51 +0900

    gg

--- src/pages/ConfigMyInfo/index.jsx
+++ src/pages/ConfigMyInfo/index.jsx
@@ -1,40 +1,7 @@
-import React, { useState } from "react";
-import { ProgressBar } from "react-bootstrap";
-
-import questions from "./questions";
-
-const ConfigMyInfo = () => {
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [answers, setAnswers] = useState({});
-
-  const handleNext = () => {
-    if (currentQuestion < questions.length - 1) {
-      setCurrentQuestion(currentQuestion + 1);
-    }
-  };
-
-  // 답변을 저장하는 함수
-  const handleChange = (e) => {
-    setAnswers({
-      ...answers,
-      [questions[currentQuestion].name]: e.target.value,
-    });
-  };
-
-  // 진행률 계산
-  const progress = ((currentQuestion + 1) / questions.length) * 100;
-
+export default function ConfigMyInfo() {
   return (
-    <>
-      <h3>OOO님에 대해 설명해주세요</h3>
-
-      <ProgressBar
-        now={progress}
-        label={`${Math.round(progress)}%`}
-        className="my-4"
-      />
-    </>
+    <div>
+      <h1>ConfigMyInfo</h1>
+    </div>
   );
-};
-
-export default ConfigMyInfo;
+}
