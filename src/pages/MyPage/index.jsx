a0342d7a9a530d372baf8f2edcc336291119c6a9
Kim, mubin <kimmubin0318@gmail.com>
2024-10-31 18:31:42 +0900

    test

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
 import style from './style.module.scss';
 import hr from './images/Line.png';
 export default function MyPage() {
+    const [profileList, setProfileList] = useState([]);
+
+    useEffect(() => {
+        const baseUrl = '/api/v1/member';
+        const endPoint = '/profile';
+
+        axios
+            .get(baseUrl + endPoint)
+            .then((res) => {
+                setProfileList(res.data);
+                console.log(res.data);
+            })
+            .catch((error) => {
+                console.error(error.response.data.message);
+            });
+    });
     return (
         <div className={style.myPage}>
             <div className={style.profile}>
@@ -16,10 +35,19 @@ export default function MyPage() {
                 <img src={hr} alt="선" />
             </div>
             <div className={style.info}>
-                <div>이름</div>
-                <div>이메일</div>
-                <div>역할</div>
-                <div>기술 스택</div>
+                <div>{/* 이름 <input type="text" value={} /> */}</div>
+                <div>
+                    이메일
+                    <input type="text" />
+                </div>
+                <div>
+                    역할
+                    <input type="text" />
+                </div>
+                <div>
+                    기술 스택
+                    <input type="text" />
+                </div>
             </div>
         </div>
     );
