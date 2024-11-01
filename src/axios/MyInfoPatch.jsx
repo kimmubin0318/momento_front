b9c1e5a689e6ab1d565e15ac0be49b400308c808
Kim, mubin <kimmubin0318@gmail.com>
2024-11-01 19:59:29 +0900

    dfaf

--- src/axios/MyInfoPatch.jsx
+++ src/axios/MyInfoPatch.jsx
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
-async function MyInfoPatch(data) {
+async function MyInfoPatch(changeData) {
+    console.log(changeData);
+
     try {
         const baseUrl = '/api/v1/member/';
         const endPoint = '/update-profile';
         const response = await axios.patch(baseUrl + endPoint, {
-            persona: data.changePersona,
-            ability: data.changeAbility,
-            stack: data.changeStack,
+            persona: changeData.changePersona,
+            ability: changeData.changeAbility,
+            stack: changeData.changeStack,
         });
         return response.data;
     } catch (error) {
