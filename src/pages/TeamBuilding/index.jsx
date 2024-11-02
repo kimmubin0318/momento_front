e05524470261f7f6e930483476dbec7a4dafc34e
dandamdandam <glue0440@gmail.com>
2024-11-02 15:57:26 +0900

    feat: 팀빌딩 스켈레톤

--- src/pages/TeamBuilding/index.jsx
+++ src/pages/TeamBuilding/index.jsx
@@ -1,7 +1,45 @@
+import { Form, Button } from "react-bootstrap";
+
 export default function TeamBuilding() {
-    return (
-        <div>
-            <h1>TeamBuilding</h1>
-        </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO Handle form submission logic here
+    console.log(
+      e.target.startDate.value,
+      e.target.endDate.value,
+      e.target.teamSize.value,
+      e.target.myPosition.value,
+      e.target.positionCombination.value
     );
+  };
+
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Control id="startDate" aria-label="startDate" type="date" />
+          <Form.Control id="endDate" aria-label="endDate" type="date" />
+        </Form.Group>
+
+        <Form.Group controlId="teamSize">
+          <Form.Label>Team Size</Form.Label>
+          <Form.Control type="number" />
+        </Form.Group>
+
+        <Form.Group controlId="myPosition">
+          <Form.Label>My Position</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+
+        <Form.Group controlId="positionCombination">
+          <Form.Label>Position Combination</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
