962226099b6055d9d3e871179c7d71447415bbd3
Kim, mubin <kimmubin0318@gmail.com>
2024-11-02 15:58:31 +0900

    test

--- src/pages/ConfigMyInfo/index.jsx
+++ src/pages/ConfigMyInfo/index.jsx
@@ -1,42 +1,7 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 export default function ConfigMyInfo() {
-    const navigate = useNavigate();
-    const [tokenReceived, setTokenReceived] = useState(false); // 토큰 여부
-    const code = new URL(window.location.href).searchParams.get('code');
-    useEffect(() => {
-        if (code) {
-            // 인가 코드를 백엔드에 보내서 토큰을 받아옴
-            axios
-                .get(`https://duoh.site/api/v1/auth/callback?code=${code}`)
-                .then((response) => {
-                    if (response.data.statusCode) {
-                        setTokenReceived(true); // 토큰 수신 완료
-                        navigate('/config-my-info');
-                    } else {
-                        console.log('로그인 실패:', response.message);
-                        navigate('/login'); // 실패 시 로그인 페이지로 이동
-                    }
-                })
-                .catch((error) => {
-                    console.log('Error:', error);
-                    navigate('/login');
-                });
-        } else {
-            console.log('인가 코드가 없습니다.');
-            navigate('/login');
-        }
-    }, [code, navigate]);
-
-    if (!tokenReceived) {
-        return <h1>로그인 중...</h1>; // 토큰 수신 전
-    }
-
     return (
-        // 후
         <div>
-            <h1>정보 입력 페이지</h1>
+            <h1>config my info</h1>
         </div>
     );
 }
