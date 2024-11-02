ffe597da1f6e6462bd18699000ba11162a7470fd
Kim, mubin <kimmubin0318@gmail.com>
2024-11-03 04:23:38 +0900

    킹두오

--- src/pages/Start/index.jsx
+++ src/pages/Start/index.jsx
@@ -1,38 +1,25 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { RouterPath } from "../../utils/path";
-import Logo from "./logo";
-import style from "./style.module.scss";
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { RouterPath } from '../../utils/path';
+import Logo from './logo';
+import style from './style.module.scss';
 export default function StartPage() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-<<<<<<< HEAD
-  // navigate 를 사용하여 3.5초 뒤에 /login 으로 이동하는 함수
-  const navigateLogin = () => {
-    setTimeout(() => {
-      navigate(RouterPath.login.getPath());
-    }, 3500);
-  };
-  // useEffect를 사용하여 페이지가 렌더링 되면 함수 시작
-  useEffect(() => {
-    navigateLogin();
-  }, []);
-=======
     // navigate 를 사용하여 3.5초 뒤에 /login 으로 이동하는 함수
     const navigateLogin = () => {
         setTimeout(() => {
-            navigate('/login');
+            navigate(RouterPath.login.getPath());
         }, 3500);
     };
     // useEffect를 사용하여 페이지가 렌더링 되면 함수 시작
     useEffect(() => {
         navigateLogin();
     }, []);
->>>>>>> 01d5d0c (아! 잠 온다)
 
-  return (
-    <div className={style.LogoContainer}>
-      <Logo />
-    </div>
-  );
+    return (
+        <div className={style.LogoContainer}>
+            <Logo />
+        </div>
+    );
 }
