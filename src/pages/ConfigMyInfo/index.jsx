b4e68a5163e0c5eabfac9bcbb9e1d7fa7bca1aae
Kim, mubin <kimmubin0318@gmail.com>
2024-11-02 11:30:43 +0900

    test

--- src/pages/ConfigMyInfo/index.jsx
+++ src/pages/ConfigMyInfo/index.jsx
@@ -5,16 +5,13 @@ export default function ConfigMyInfo() {
     const navigate = useNavigate();
     const [tokenReceived, setTokenReceived] = useState(false); // 토큰 여부
     const code = new URL(window.location.href).searchParams.get('code');
-
     useEffect(() => {
         if (code) {
             // 인가 코드를 백엔드에 보내서 토큰을 받아옴
             axios
                 .get(`https://duoh.site/api/v1/auth/callback?code=${code}`)
                 .then((response) => {
-                    console.log('Access Token:', response.data.accessToken);
-                    console.log('Refresh Token:', response.data.refreshToken);
-                    if (response.statusCode === 200) {
+                    if (response.status === 200) {
                         setTokenReceived(true); // 토큰 수신 완료
                         navigate('/config-my-info');
                     } else {
