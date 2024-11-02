d254414549752b77cc02c5db5d001a832a863755
Kim, mubin <kimmubin0318@gmail.com>
2024-11-03 04:18:59 +0900

    최신

--- src/pages/Start/index.jsx
+++ src/pages/Start/index.jsx
@@ -6,6 +6,7 @@ import style from "./style.module.scss";
 export default function StartPage() {
   const navigate = useNavigate();
 
+<<<<<<< HEAD
   // navigate 를 사용하여 3.5초 뒤에 /login 으로 이동하는 함수
   const navigateLogin = () => {
     setTimeout(() => {
@@ -16,6 +17,18 @@ export default function StartPage() {
   useEffect(() => {
     navigateLogin();
   }, []);
+=======
+    // navigate 를 사용하여 3.5초 뒤에 /login 으로 이동하는 함수
+    const navigateLogin = () => {
+        setTimeout(() => {
+            navigate('/login');
+        }, 3500);
+    };
+    // useEffect를 사용하여 페이지가 렌더링 되면 함수 시작
+    useEffect(() => {
+        navigateLogin();
+    }, []);
+>>>>>>> 01d5d0c (아! 잠 온다)
 
   return (
     <div className={style.LogoContainer}>
